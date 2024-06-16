da6330d5004d0f11b44b47fac8c98615f2d0da6e
LeonardoSbrolini <leonardosbrolinirodrigues2002@gmail.com>
2024-06-16 16:29:15 -0300

    Testando variaveis de ambiente

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@
 export default function Home() {
   return (
     <>
+      <div>NEXTAUTH_URL: {process.env.NEXTAUTH_URL}</div>
+      <div>NEXTAUTH_SECRET: {process.env.NEXTAUTH_SECRET}</div>
+      <div>DATABASE_URL: {process.env.DATABASE_URL}</div>
+      <div>NEXT_PUBLIC_API_URL: {process.env.NEXT_PUBLIC_API_URL}</div>
     </>
   )
 }
